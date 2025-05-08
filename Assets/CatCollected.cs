54cccd1afaea7db096903d90abde7268a3b64e94
hyakaera <alinattrinh@gmail.com>
2025-05-07 20:52:29 -0400

    Cat Counter

--- Assets/CatCollected.cs
+++ Assets/CatCollected.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class CatCollected : MonoBehaviour
 {
-
+      private AudioSource audioSource;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+
+          if (audioSource != null && audioSource.clip != null)
+            {
+                audioSource.Play();
+            }
             // Add to counter
             CatCollector.Instance.AddCat();
 
             // Remove cat
-            Destroy(gameObject);
+            Destroy(gameObject, 1f);
         }
     }
 }
